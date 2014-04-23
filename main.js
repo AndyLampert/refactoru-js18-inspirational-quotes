b74a1b8cb350732715a482842a7d5e07f056768b
AndyLampert <lamperta@gmail.com>
2014-04-23 15:40:19 -0600

    added temporary delete icon

--- main.js
+++ main.js
@@ -1,33 +1,89 @@
+// step 1
+// make a function that takes an object (quote object)
+// that creates a quote div
+// creates an author div
+// using the values in the quote object
+// add to the DOM
+
+// biggest change is instead of retriving from input fields
+// to retrieve from the data object
+
+// step 2
+// extract the functionality of creating quotes and adding them to the DOM
+// and call that func for each quote in data object.
+
+var data = [{
+		author: 'Bronn',
+		quote: 'You waste time trying to get people to love you, you’ll end up the most popular dead man in town.'
+	},
+	{
+		author: 'Yggrit',
+		quote: 'You\'re mine as I am yours. And if we die, we die. But first, we’ll live.'
+	},
+	{
+		author: 'Tyrion',
+		quote: 'Even a stunted, twisted, ugly little boy can look down over the world when he\'s seated on a dragon\'s back.'
+	}
+]
+
+
+
 $(document).on('ready', function() {
+	// delete icon
+	$('.fa').on('click', function(){
+		// delete the .single-quote-container associated with this icon
+		$('.single-quote-container').remove();
+	});
+	// submit button
 	$('#submit-button').on('click', function(){
 		// take the #input-quote
 		var quoteVal = $('#input-quote').val();
 		// create new .quote div
 		var newQuoteDiv = $('<div class="quote"></div>');
 		// set its text to #input-quote
-		newQuoteDiv.text('hi2')
+		var newDeleteIcon = $('<i class="fa fa-camera-retro"></i>');
+		newQuoteDiv.text(quoteVal);
 		$('.main-quotes-container').append(newQuoteDiv);
+		$('.single-quote-container').append(newDeleteIcon);
+
+		$('body').append(newDeleteIcon);
+
+		// make sure #input-quote, #input-author is filled in
+		if(quoteVal.length === 0){
+			console.log("didn't input in quote field");
+		} 
 
 		// take the #input-author
 		var authorVal = $('#input-author').val();
 		// create new .author div
 		var newAuthorDiv = $('<div class="author"></div>');
 		// set text of new .author to #input-author input
-		newAuthorDiv.text('hi');
+		newAuthorDiv.text(authorVal);
 		$('.main-quotes-container').append(newAuthorDiv);
 
 		return false;
 	});
+
+
+// This list should update in real-time when quotes are added, 
+// deleted, or rated.
+
 	// checking for a valid input
 	// var isValid = validateInput($('[data-validation="required"]'));	  
 });
 
-// NOTES
-// You have some Javascript data (primitive value, array, object, etc)
-// var numDocs = 10;
+// Next step - validate input fields
 
-// Use jQuery to create a new DOM element.
-// var messageEl = $("<p>You have {0} documents.</p>".supplant([numDocs]));
+// step 4
+// User can click on the author of a quote to go to a separate 
+// page/sceeen that shows all quotes from that person.
 
-// Add the DOM element to the page.
-// $("body").append(messageEl);
+// var validateInput = function(input) {
+// 		var allValid = true;
+// 		input.each(function() {
+// 			var inputValid = $(this).val() !== '';
+// 			$(this).closest('.form-group').addClass(inputValid ? 'valid' : 'invalid');
+// 			allValid &= inputValid;
+// 		})
+// 		return !!allValid;
+// 	};
