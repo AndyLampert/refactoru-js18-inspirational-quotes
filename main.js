1f0b6c411a06f590879de8bf49cbe8e8bd550f7d
AndyLampert <lamperta@gmail.com>
2014-04-23 11:29:57 -0600

    set up html and css

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).on('ready', function() {
-  
+
+	// checking for a valid input
+	var isValid = validateInput($('[data-validation="required"]'));	  
 });
+
+// NOTES
+// You have some Javascript data (primitive value, array, object, etc)
+// var numDocs = 10;
+
+// Use jQuery to create a new DOM element.
+// var messageEl = $("<p>You have {0} documents.</p>".supplant([numDocs]));
+
+// Add the DOM element to the page.
+// $("body").append(messageEl);
