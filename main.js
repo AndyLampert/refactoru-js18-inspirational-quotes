16d1f90ffe01386cba30bf2b9d887ae2873d177d
AndyLampert <lamperta@gmail.com>
2014-04-23 12:17:14 -0600

    set up quote append functionality

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 $(document).on('ready', function() {
 	$('#submit-button').on('click', function(){
-		// take the #input-author
 		// take the #input-quote
-		// put them into .main-quotes-container
+		var quoteVal = $('#input-quote').val();
+		// create new .quote div
+		var newQuoteDiv = $('<div class="quote"></div>');
+		// set its text to #input-quote
+		newQuoteDiv.text('hi2')
+		$('.main-quotes-container').append(newQuoteDiv);
+
+		// take the #input-author
+		var authorVal = $('#input-author').val();
+		// create new .author div
+		var newAuthorDiv = $('<div class="author"></div>');
+		// set text of new .author to #input-author input
+		newAuthorDiv.text('hi');
+		$('.main-quotes-container').append(newAuthorDiv);
+
+		return false;
 	});
 	// checking for a valid input
-	var isValid = validateInput($('[data-validation="required"]'));	  
+	// var isValid = validateInput($('[data-validation="required"]'));	  
 });
 
 // NOTES
